258fb3fbb27562227be808a3ff5a0a48c0cb456d
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2025-12-20 21:46:51 +0330

    adding jump and sit

--- 2dPlatformer/Assets/Scripts/PlayerMovement.cs
+++ 2dPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float walkSpeed = 2f;
-    [SerializeField] private float runSpeed = 4f;
+    [Header("Movement")]
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float sitWalkSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCD;
 
-    [Range(0f, 1f)]
+    [Header("Joystick Thresholds")]
     [SerializeField] private float walkThreshold = 0.3f;
+    [SerializeField] private float jumpSitThreshold = 0.5f;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckDistance = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private Joystick joystick;
     private Animator animator;
+    private bool canJump = true;
+    private bool isSitting = false;
+
+    private bool isGrounded;
 
     void Start()
     {
@@ -21,40 +35,88 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        GroundCheck();
+
         float h = joystick.Horizontal;
+        float v = joystick.Vertical;
+
         float absH = Mathf.Abs(h);
+        float speed = absH < 0.7f ? walkSpeed : runSpeed;
+
+        if(v < -jumpSitThreshold)
+        {
+            isSitting = true;
+            // speed = sitWalkSpeed;
+        } else
+        {
+            isSitting = false;
+        }
+        animator.SetBool("isSitting", isSitting);
 
         if (absH < walkThreshold)
         {
             rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
             animator.SetBool("isRunning", false);
             animator.SetBool("isWalking", false);
-            return;
+        }
+        else
+        {
+            if(isSitting)
+                rb.linearVelocity = new Vector2(Mathf.Sign(h) * sitWalkSpeed, rb.linearVelocity.y);
+            else
+                rb.linearVelocity = new Vector2(Mathf.Sign(h) * speed, rb.linearVelocity.y);
+
+            animator.SetBool("isWalking", speed == walkSpeed);
+            animator.SetBool("isRunning", speed == runSpeed);
+
+            Flip(h);
         }
 
-        float speed = absH < 0.7f ? walkSpeed : runSpeed;
-        rb.linearVelocity = new Vector2(Mathf.Sign(h) * speed, rb.linearVelocity.y);
-
-        // rotate character 180 deg in x
-        Flip(h);
-
-        // animation state manager
-        if (speed == walkSpeed)
+        if (v > jumpSitThreshold && isGrounded && canJump)
         {
-            animator.SetBool("isRunning", false);
-            animator.SetBool("isWalking", true);
-        } else if (speed == runSpeed)
-        {
-            animator.SetBool("isRunning", true);
-            animator.SetBool("isWalking", false);
+            Vector2 jumpDir = new Vector2(0, 1f).normalized;
+
+            rb.AddForce(jumpDir * jumpForce, ForceMode2D.Impulse);
+            animator.SetTrigger("jump");
+            isGrounded = false;
+            canJump = false;
+            Invoke(nameof(ResetJumpCD), jumpCD);
         }
     }
 
     void Flip(float h)
     {
         if (h > 0.05f)
-            transform.localScale = new Vector3(3.5f, transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(3f, transform.localScale.y, transform.localScale.z);
         else if (h < -0.05f)
-            transform.localScale = new Vector3(-3.5f, transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(-3f, transform.localScale.y, transform.localScale.z);
+    }
+
+    void GroundCheck()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(
+            groundCheckPoint.position,
+            Vector2.down,
+            groundCheckDistance,
+            groundLayer
+        );
+
+        isGrounded = hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (groundCheckPoint == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(
+            groundCheckPoint.position,
+            groundCheckPoint.position + Vector3.down * groundCheckDistance
+        );
+    }
+
+    private void ResetJumpCD()
+    {
+        canJump = true;
     }
 }
